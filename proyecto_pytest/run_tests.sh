f98bc409794d5222590068a2253dcb577140b3b1
joserichart <joserichart@gmail.com>
2026-03-02 22:11:37 -0500

    cambio instalador paquetes

--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -5,8 +5,8 @@ python3 -m venv venv
 source venv/Scripts/activate
 
 echo "instalando dependencias"
-pip install --upgrade pip
-pip install -r requirements.txt
+pipx install --upgrade pip
+pipx install -r requirements.txt
 
 echo "ejecutando pruebas con pytest"
 pytest tests/  --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html

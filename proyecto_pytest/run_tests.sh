08769163eb397ecfa6d2b93b06955fa570057738
joserichart <joserichart@gmail.com>
2026-03-02 22:34:20 -0500

    cambios dependencias

--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-echo "activando el entorno virtual"
-python3 -m venv venv
-source venv/Scripts/activate
+echo " Iniciando ejecucion de pruebas en Jenkis"
 
+# Verificar si el entorno virtual existe
+if [ ! -d "venv" ]; then
+    echo "Entorno virtual no encontrado. Creandolo"
+    python3 -m venv venv
+fi
+
+# Activar el entorno virtual correctamente
+if [ -f "venv/bin/activate" ]; then
+    source venv/bin/activate
+elseif [ -f "venv/Scripts/activate" ]; then # Para Windows
+    source venv/Scripts/activate
+else
+    echo " Error: No se pudo activar el entorno virtual"
+    exit 1
+fi
+
+# Verificar si pip esta instalado correctamente
 echo "instalando dependencias"
-pip3 install --upgrade pip
-pip3 install -r requirements.txt
 
-echo "ejecutando pruebas con pytest"
-pytest tests/  --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
+pip install --upgrade pip --braak-system-packages
+pip install -r requirements.txt --break-system-packages
+
+# ejecutar las pruebas
+echo " Ejecutando Pruebas con Pytest"
+venv/bin/python -m pytest tests/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
 
 echo "pruebas finalizadas resultados en reports"

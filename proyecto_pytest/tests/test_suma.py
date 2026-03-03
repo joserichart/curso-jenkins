ba37554b69733896beae3fed67f2b28a158957d8
joserichart <joserichart@gmail.com>
2026-03-02 21:18:04 -0500

    proyecto python

---
+++ proyecto_pytest/tests/test_suma.py
@@ -0,0 +1,14 @@
+import pytest
+
+def suma(a, b):
+    """Funcion que suma dos numeros"""
+    return a + b
+
+def test_suma():
+    assert suma(1, 2) == 3
+    assert suma(-1, 1) == 0
+    assert suma(0, 0) == 0
+
+def test_suma_fail():
+    assert suma(1, 2) == 4
+

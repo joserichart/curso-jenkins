6c44ef5dface147b658b040b7a9c8098b5fd6929
joserichart <joserichart@gmail.com>
2026-03-02 22:13:35 -0500

    cambio comando pip

--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -5,8 +5,8 @@ python3 -m venv venv
 source venv/Scripts/activate
 
 echo "instalando dependencias"
-pipx install --upgrade pip
-pipx install -r requirements.txt
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
 
 echo "ejecutando pruebas con pytest"
 pytest tests/  --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html

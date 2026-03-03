85bf628d14acd21af3afaa97c6775019777a6c76
joserichart <joserichart@gmail.com>
2026-03-02 21:24:35 -0500

    Agregando paso para ingresar al directorio de pytest

--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Ingresando al proyecto"
+cd proyecto_pytest
+
 echo "activando el entorno virtual"
 source venv/bin/activate
 
@@ -7,6 +10,6 @@ echo "instalando dependencias"
 pip install -r requirements.txt
 
 echo "ejecutando pruebas con pytest"
-pytest test/  --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
+pytest tests/  --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
 
 echo "pruebas finalizadas resultados en reports"
